57d5635a6de4241dc96fee4e43588065b492ea41
Leon Lee <93leonlee@gmail.com>
2014-02-03 01:31:00 -0500

    add description

--- Text/wordcount.py
+++ Text/wordcount.py
@@ -1,3 +1,8 @@
+'''
+Counts the number of individual words in a string.
+For added complexity read these strings in from a text
+file and generate a summary.
+'''
 def count_words(string='', file=None):
 	if file:
 		try:
